534c947fd048c92abea53f1bf08df8fae908d361
chase <chaselbjeter@gmail.com>
2026-03-28 13:55:57 -0400

    Revert "feat(store): implement cursor, block, raw event, and auction repositories (#69)"
    This reverts commit eaf17e81932b8f2f82b552c0a6b05a88dcd3a874.

--- internal/store/postgres/block.go
+++ internal/store/postgres/block.go
@@ -1,46 +1,17 @@
 package postgres
 
-import (
-	"context"
-	"errors"
-
-	"github.com/jackc/pgx/v5"
-)
+import "context"
 
 type blockRepo struct{ q querier }
 
 func (r *blockRepo) Insert(ctx context.Context, chainID int64, blockNumber uint64, blockHash, parentHash string) error {
-	_, err := r.q.Exec(ctx,
-		`INSERT INTO indexed_blocks (chain_id, block_number, block_hash, parent_hash)
-		 VALUES ($1, $2, $3, $4)
-		 ON CONFLICT DO NOTHING`,
-		chainID, blockNumber, blockHash, parentHash,
-	)
-	return err
+	panic("not implemented")
 }
 
 func (r *blockRepo) GetHash(ctx context.Context, chainID int64, blockNumber uint64) (string, error) {
-	var hash string
-
-	err := r.q.QueryRow(ctx,
-		"SELECT block_hash FROM indexed_blocks WHERE chain_id = $1 AND block_number = $2",
-		chainID, blockNumber,
-	).Scan(&hash)
-
-	if errors.Is(err, pgx.ErrNoRows) {
-		return "", nil
-	}
-	if err != nil {
-		return "", err
-	}
-
-	return hash, nil
+	panic("not implemented")
 }
 
 func (r *blockRepo) DeleteFrom(ctx context.Context, chainID int64, fromBlock uint64) error {
-	_, err := r.q.Exec(ctx,
-		"DELETE FROM indexed_blocks WHERE chain_id = $1 AND block_number >= $2",
-		chainID, fromBlock,
-	)
-	return err
+	panic("not implemented")
 }
--- internal/store/postgres/cursor.go
+++ internal/store/postgres/cursor.go
@@ -1,42 +1,13 @@
 package postgres
 
-import (
-	"context"
-	"errors"
-
-	"github.com/jackc/pgx/v5"
-)
+import "context"
 
 type cursorRepo struct{ q querier }
 
 func (r *cursorRepo) Get(ctx context.Context, chainID int64) (uint64, string, error) {
-	var blockNumber uint64
-	var blockHash string
-
-	err := r.q.QueryRow(ctx,
-		"SELECT last_block, last_block_hash FROM indexer_cursors WHERE chain_id = $1",
-		chainID,
-	).Scan(&blockNumber, &blockHash)
-
-	if errors.Is(err, pgx.ErrNoRows) {
-		return 0, "", nil
-	}
-	if err != nil {
-		return 0, "", err
-	}
-
-	return blockNumber, blockHash, nil
+	panic("not implemented")
 }
 
 func (r *cursorRepo) Upsert(ctx context.Context, chainID int64, blockNumber uint64, blockHash string) error {
-	_, err := r.q.Exec(ctx,
-		`INSERT INTO indexer_cursors (chain_id, last_block, last_block_hash)
-		 VALUES ($1, $2, $3)
-		 ON CONFLICT (chain_id) DO UPDATE
-		 SET last_block = EXCLUDED.last_block,
-		     last_block_hash = EXCLUDED.last_block_hash,
-		     updated_at = NOW()`,
-		chainID, blockNumber, blockHash,
-	)
-	return err
+	panic("not implemented")
 }
